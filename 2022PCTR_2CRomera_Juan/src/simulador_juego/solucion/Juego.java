515b7bcc712febd37432c816403392e29bded804
romer <romer@DESKTOP-P19K0PI>
2022-06-21 19:48:10 +0200

    Más funciones completadas

--- 2022PCTR_2CRomera_Juan/src/simulador_juego/solucion/Juego.java
+++ 2022PCTR_2CRomera_Juan/src/simulador_juego/solucion/Juego.java
@@ -1,5 +1,6 @@
 package simulador_juego.solucion;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class Juego implements IJuego{
@@ -21,13 +22,51 @@ public class Juego implements IJuego{
 	}
 
 	@Override
-	public void generarEnemigo(int tipoEnemigo) {
+	public synchronized void generarEnemigo(int tipoEnemigo) {
 		
 	}
 
 	@Override
-	public void eliminarEnemigo(int tipoEnemigo) {
+	public synchronized void eliminarEnemigo(int tipoEnemigo) {
 		
 	}
 	
+	public void imprimirInfo(int tipo, String accion) {
+		System.out.println(accion+" enemigo de tipo "+tipo);
+		System.out.println("--> Enemigos totales: "+contadorEnemigosTotales);
+		
+		Enumeration<Integer> ids=contadoresEnemigosTipo.keys();
+		int id;
+		while(ids.hasMoreElements()) {
+			id=ids.nextElement();
+			System.out.println("----> Enemigos tipo "+id+": "+contadoresEnemigosTipo.get(id)+"------ [Eliminados: "+contadoresEliminadosTipo.get(id)+"]");
+		}
+	}
+	
+	public int sumarContadores() {
+		Integer temp=0;
+		
+		Enumeration<Integer> contadores=contadoresEnemigosTipo.elements();
+		while(contadores.hasMoreElements()) {
+			temp+=contadores.nextElement();
+		}
+		
+		return temp;
+	}
+	
+	protected void checkInvariante() {
+		assert sumarContadores()==contadorEnemigosTotales;
+	}
+	
+	protected void comprobarAntesDeGenerar(int tipoEnemigo) {
+		if(tipoEnemigo==0) {
+			assert contadorEnemigosTotales < MAXENEMIGOS : "No se ha llegado al limite de enemigos";
+		}else {
+			assert contadorEnemigosTotales < MAXENEMIGOS && (contadoresEnemigosTipo.get(tipoEnemigo-1)>0||contadoresEliminadosTipo.get(tipoEnemigo-1)>0) : "No se ha llegado al limite de enemigos y se han empezado a generar enemigos del tipo anterior";
+		}
+	}
+	
+	protected void comprobarAntesDeEliminar(int tipoEnemigo) {
+		assert contadoresEnemigosTipo.get(tipoEnemigo)>0 : "Hay al menos un enemigo del tipo indicado para eliminar";
+	}
 }
--- 2022PCTR_2CRomera_Juan/src/simulador_juego/solucion/SistemaLanzador.java
+++ 2022PCTR_2CRomera_Juan/src/simulador_juego/solucion/SistemaLanzador.java
@@ -1,5 +1,12 @@
 package simulador_juego.solucion;
 
 public class SistemaLanzador {
+	
+	private static int[] numEnemigosPorTipo= {4, 3, 2, 1};
+	private static final int MAXENEMIGOS=5;
 
+	public static void main(String[] args) {
+		Juego juego=new Juego(numEnemigosPorTipo.length, MAXENEMIGOS);
+	}
+	
 }
